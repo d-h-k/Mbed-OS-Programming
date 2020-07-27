37f925b57da7f0882468d581685bb730c092db69
DHkim <ultraraptor@naver.com>
2020-07-27 19:55:42 +0900

    update

---
+++ ubuntu20_mbedcli.sh
@@ -0,0 +1,10 @@
+sudo apt install python3-dev
+sudo apt-get install git
+sudo apt-get install build-essential 
+sudo apt install python2.7
+sudo apt install mercurial
+sudo pip3 install mbed-cli
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+
